3665efd5dcbabf537905391eb48c73aad9d9bf23
orbeet <excenter8@gmail.com>
2022-06-23 19:35:06 +0300

    About Us

--- scrypt.js
+++ scrypt.js
@@ -36,12 +36,14 @@ if (isMobile.any()) {
 // menu burger
 const iconMenu = document.querySelector(".menu__icon");
 const menuBody = document.querySelector(".menu__body");
+const noScroll = document.querySelector(".w-nav-button");
 const mobileBtn = document.querySelector(".mobile__burger-btn");
 if (iconMenu) {
   iconMenu.addEventListener("click", function (e) {
     document.body.classList.add("_lock");
     iconMenu.classList.add("_active");
     menuBody.classList.add("_active");
+    noScroll.classList.add("w--open");
   });
 }
 
@@ -50,11 +52,36 @@ if (mobileBtn) {
     document.body.classList.remove("_lock");
     iconMenu.classList.remove("_active");
     menuBody.classList.remove("_active");
+    noScroll.classList.remove("w--open");
   });
 }
 
+// Stop body scroll when mobile menu is open
+const body = document.body;
+function letBodyScroll(bool) {
+  if (bool) {
+    body.style.overflow = "hidden";
+  } else {
+    body.style.overflow = "auto";
+  }
+}
+
+const targetNode = document.querySelector(".w-nav-button");
+const config = { attributes: true, childList: false, subtree: false };
+const callback = function (mutationsList, observer) {
+  for (let i = 0; i < mutationsList.length; i++) {
+    if (mutationsList[i].type === "attributes") {
+      const menuIsOpen = mutationsList[i].target.classList.contains("w--open");
+      letBodyScroll(menuIsOpen);
+    }
+  }
+};
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
+
 // scroll on click
-const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
+// const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
+const menuLinks = document.querySelectorAll(".goto[data-goto]");
 if (menuLinks.length > 0) {
   menuLinks.forEach((menuLink) => {
     menuLink.addEventListener("click", onMenuLinkClick);
@@ -76,6 +103,7 @@ if (menuLinks.length > 0) {
         document.body.classList.remove("_lock");
         iconMenu.classList.remove("_active");
         menuBody.classList.remove("_active");
+        noScroll.classList.remove("w--open");
       }
 
       window.scrollTo({
@@ -86,3 +114,24 @@ if (menuLinks.length > 0) {
     }
   }
 }
+
+$(document).ready(function () {
+  $(function () {
+    //BEGIN
+    $(".accordion__title").on("click", function (e) {
+      e.preventDefault();
+      var $this = $(this);
+
+      if (!$this.hasClass("accordion-active")) {
+        $(".accordion__content").slideUp(400);
+        $(".accordion__title").removeClass("accordion-active");
+        $(".accordion__arrow").removeClass("accordion__rotate");
+      }
+
+      $this.toggleClass("accordion-active");
+      $this.next().slideToggle();
+      $(".accordion__arrow", this).toggleClass("accordion__rotate");
+    });
+    //END
+  });
+});
