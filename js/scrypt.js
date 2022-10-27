be8928c2f3a0ac99ef0c587c55b39bdbfb9cf7b1
orbeet <excenter8@gmail.com>
2022-10-27 23:57:51 +0300

    slider1 add webp img

--- js/scrypt.js
+++ js/scrypt.js
@@ -1549,3 +1549,13 @@ let galleryTop = new Swiper(".gallery-top", {
 //     animOnScroll();
 //   }, 400);
 // }
+// var scrollHeight = Math.max(
+//   document.body.scrollHeight,
+//   document.documentElement.scrollHeight,
+//   document.body.offsetHeight,
+//   document.documentElement.offsetHeight,
+//   document.body.clientHeight,
+//   document.documentElement.clientHeight
+// );
+
+// alert("Высота с учётом прокрутки: " + scrollHeight);
